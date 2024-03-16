39d4f9ba01ca3687693db0e84d29a74c6c1ec25e
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-16 10:10:05 -0700

    Ajustes

---
+++ Agencia_Fiscal/src/main/java/org/itson/proyectoBDA/agencia_fiscal/Agencia_Fiscal.java
@@ -0,0 +1,14 @@
+
+package org.itson.proyectoBDA.agencia_fiscal;
+
+
+public class Agencia_Fiscal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
