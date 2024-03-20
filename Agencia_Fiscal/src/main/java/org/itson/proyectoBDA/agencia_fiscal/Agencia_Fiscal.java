a66db8de624c8e1f8d91b93905ea18d6408585be
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2024-03-19 21:19:56 -0700

    Olvida el ultimo commit que hice, ahora si jala el mapeo
    este es el chilo

--- Agencia_Fiscal/src/main/java/org/itson/proyectoBDA/agencia_fiscal/Agencia_Fiscal.java
+++ Agencia_Fiscal/src/main/java/org/itson/proyectoBDA/agencia_fiscal/Agencia_Fiscal.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package org.itson.proyectoBDA.agencia_fiscal;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/**
- *
- * @author Ramosz
- */
 public class Agencia_Fiscal {
 
     public static void main(String[] args) {
@@ -22,6 +14,7 @@ public class Agencia_Fiscal {
         entityManager.getTransaction().commit();
         entityManager.close();
         entityManagerFactory.close();
+
     }
 
 }
