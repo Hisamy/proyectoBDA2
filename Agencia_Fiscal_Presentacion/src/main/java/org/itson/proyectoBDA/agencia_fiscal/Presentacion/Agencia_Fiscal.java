d0549489a39ffa82f7c3951117335163ba146096
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-23 19:36:41 -0700

    main corregida

--- Agencia_Fiscal_Presentacion/src/main/java/org/itson/proyectoBDA/agencia_fiscal/Presentacion/Agencia_Fiscal.java
+++ Agencia_Fiscal_Presentacion/src/main/java/org/itson/proyectoBDA/agencia_fiscal/Presentacion/Agencia_Fiscal.java
@@ -18,83 +18,85 @@ public class Agencia_Fiscal {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Agencia_Fiscal_PU");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
+      EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Agencia_Fiscal_PU");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
 
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        entityManagerFactory.close();
-        
+    // Crear clientes
+    Cliente cliente1 = new Cliente(
+            "TARE040818HSRLMDA1",
+            "Eduardo",
+            "Talavera",
+            "Ramos",
+            true,
+            "RETADGAG12344",
+            "6442261760",
+            new GregorianCalendar(2004, 8, 18));
 
-       
+    Cliente cliente2 = new Cliente(
+            "CICH191004HSRLMDA",
+            "Hisamy",
+            "Cinco",
+            "Cota",
+            false,
+            "HOLA1234",
+            "6871699800",
+            new GregorianCalendar(2004, 10, 19));
 
-        Cliente cliente1 = new Cliente(
-                "TARE040818HSRLMDA1", 
-                "Eduardo", 
-                "Talavera", 
-                "Ramos", 
-                true, 
-                "RETADGAG12344", 
-                "6442261760", 
-                new GregorianCalendar(2004, 8, 18));
-        
-         Cliente cliente2 = new Cliente(
-                "CICH191004HSRLMDA", 
-                "Hisamy", 
-                "Cinco", 
-                "Cota", 
-                false, 
-                "HOLA1234", 
-                "6871699800", 
-                new GregorianCalendar(2004, 10, 19));
-         
-          Cliente cliente3 = new Cliente(
-                "CAMG040802", 
-                "Gael", 
-                "Catro", 
-                "Molina", 
-                true, 
-                "GETO18921", 
-                "6441699800", 
-                new GregorianCalendar(2004, 8, 02));
-          
-          Cliente cliente4 = new Cliente(
-                "CAMG040802", 
-                "Gael", 
-                "Catro", 
-                "Molina", 
-                true, 
-                "GETO18921", 
-                "6441699800", 
-                new GregorianCalendar(2004, 8, 02));
-          
-           Cliente cliente5 = new Cliente(
-                "VEBV040902", 
-                "Victoria", 
-                "Vega", 
-                "Bernal", 
-                true, 
-                "MIKU09211", 
-                "6871741035", 
-                new GregorianCalendar(2004, 9, 02));
-           
-            Cliente cliente6 = new Cliente(
-                "CICA120412", 
-                "Angel", 
-                "Cinco", 
-                "Cota", 
-                false, 
-                "BEIS0983211", 
-                "687211913", 
-                new GregorianCalendar(2012, 4, 12));
+    Cliente cliente3 = new Cliente(
+            "CAMG040802",
+            "Gael",
+            "Catro",
+            "Molina",
+            true,
+            "GETO18921",
+            "6441699800",
+            new GregorianCalendar(2004, 8, 02));
 
+    Cliente cliente4 = new Cliente(
+            "CAMG040802",
+            "Gael",
+            "Catro",
+            "Molina",
+            true,
+            "GETO18921",
+            "6441699800",
+            new GregorianCalendar(2004, 8, 02));
+
+    Cliente cliente5 = new Cliente(
+            "VEBV040902",
+            "Victoria",
+            "Vega",
+            "Bernal",
+            true,
+            "MIKU09211",
+            "6871741035",
+            new GregorianCalendar(2004, 9, 02));
+
+    Cliente cliente6 = new Cliente(
+            "CICA120412",
+            "Angel",
+            "Cinco",
+            "Cota",
+            false,
+            "BEIS0983211",
+            "687211913",
+            new GregorianCalendar(2012, 4, 12));
 
     
-//        IRegistroClientesBO registraClientes = new RegistroClientesBO();
-//        RegistrarCliente registraCliente = new RegistrarCliente(registraClientes);
-        IConsultaClientesBO consultaClientes = new ConsultaClientesBO();
-        BuscarCliente buscarCliente = new BuscarCliente(consultaClientes);
-        buscarCliente.setVisible(true);
+    entityManager.persist(cliente1);
+    entityManager.persist(cliente2);
+    entityManager.persist(cliente3);
+    entityManager.persist(cliente4);
+    entityManager.persist(cliente5);
+    entityManager.persist(cliente6);
+
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    entityManagerFactory.close();
+
+    IConsultaClientesBO consultaClientes = new ConsultaClientesBO();
+    BuscarCliente buscarCliente = new BuscarCliente(consultaClientes);
+    buscarCliente.setVisible(true);
     }
 }
